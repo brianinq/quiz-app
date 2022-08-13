caff856bbc79e9afb8c3d9f103630a22b99cc4c7
brian <kipkoechbrian0@gmail.com>
2022-08-13 18:49:56 +0300

    add questions activity ui

--- app/src/main/java/com/example/quiz/MainActivity.kt
+++ app/src/main/java/com/example/quiz/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.quiz
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import com.example.quiz.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -13,6 +15,14 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
         window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
 
-
+        binding.btnStart.setOnClickListener{
+            if(binding.etName.text.toString().isEmpty()){
+                Toast.makeText(this, "Please enter your name", Toast.LENGTH_SHORT).show()
+            }else{
+                val intent = Intent(this, QuestionsActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+        }
     }
 }
--- app/src/main/java/com/example/quiz/QuestionsActivity.kt
+++ app/src/main/java/com/example/quiz/QuestionsActivity.kt
@@ -2,10 +2,29 @@ package com.example.quiz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.example.quiz.databinding.ActivityQuestionsBinding
 
 class QuestionsActivity : AppCompatActivity() {
+    lateinit var binding: ActivityQuestionsBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityQuestionsBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_questions)
+        setContentView(binding.root)
+        val questionsList = Constants.getQuestions()
+//        Log.d("Questions", "${questionsList.size}")
+
+        val position = 1
+        val question: Question? = questionsList[position -1]
+
+        binding.progressBar.progress = position
+        binding.tvProgress.text = "${position} / ${binding.progressBar.max}"
+
+        binding.tvQuestion.text = question!!.question
+        binding.ivImage.setImageResource(question.image)
+        binding.tvOptionOne.text = question!!.optionOne
+        binding.tvOptionTwo.text = question!!.optionTwo
+        binding.tvOptionThree.text = question!!.optionThree
+        binding.tvOptionFour.text = question!!.optionFour
     }
 }
